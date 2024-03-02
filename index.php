bca11dfa8d7ca147e1506fb1951b74841c7b1982
Darshan Agravat <darshanr.agravat@gmail.com>
2024-03-02 23:50:02 +0530

    Coming Soon

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$status = "false";
+?>
+
+<?php
+if($status == "false") {
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comming Soon!</title>
+    <link rel="stylesheet" href="css/main.css">
+    
+</head>
+<body id="coming_soon_page">
+
+</body>
+</html>
+<?php
+} else {
+?>
+
+<?
+}
+?>
